840b7e0bcb8a8ee95f9dc24836a4e96a80c4d66b
Nikhil Benesch <nikhil.benesch@gmail.com>
2020-06-13 20:09:01 -0400

    libkrb5: upgrade to v1.18.2

--- krb5/src/patchlevel.h
+++ krb5/src/patchlevel.h
@@ -51,7 +51,7 @@
  */
 #define KRB5_MAJOR_RELEASE 1
 #define KRB5_MINOR_RELEASE 18
-#define KRB5_PATCHLEVEL 1
+#define KRB5_PATCHLEVEL 2
 /* #undef KRB5_RELTAIL */
-#define KRB5_RELDATE "20200413"
-#define KRB5_RELTAG "krb5-1.18.1-final"
+#define KRB5_RELDATE "20200521"
+#define KRB5_RELTAG "krb5-1.18.2-final"
